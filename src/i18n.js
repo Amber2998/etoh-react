51ea50d7637d85eebcf6d1aec856001e575c5847
nickkumpen <nick.kumpen@xcomm.be>
2021-03-12 23:17:36 +0100

    [] Add language autodetector.

--- src/i18n.js
+++ src/i18n.js
@@ -1,4 +1,5 @@
 import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
 import { reactI18nextModule } from "react-i18next";
 
 import translationNL from './locales/nl/translation.json';
@@ -15,6 +16,7 @@ const resources = {
 };
 
 i18n
+	.use(detector)
 	.use(reactI18nextModule) // passes i18n down to react-i18next
 	.init({
 		resources,
