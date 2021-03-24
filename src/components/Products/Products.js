cfa988f684dd64c0580072e15ab7f02b0a170783
Amber Claes <amber.claes@hotmail.com>
2021-03-24 11:34:14 +0100

    fixedit

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,11 +11,11 @@ const Products = ({t}) => {
 
 			<div className="card" style={{margin: "1em 0"}}>
 				<div className="row">
-					<div className="d-flex justify-content-center col-4">
+					<div className="d-flex justify-content-center col-5">
 						<img src="Images/Rabarber.png" className="card-img" alt="Lady Barbara"
 							 style={{height: "14em", width: 'auto', objectFit: "cover"}}/>
 					</div>
-					<div className="col-8">
+					<div className="col-7">
 						<div className="card-body">
 							<div className="card-title d-flex flex-row-reverse">
 								<span style={{color: "#0D9816", fontWeight: "bolder", fontSize: "24px"}}>
@@ -26,7 +26,7 @@ const Products = ({t}) => {
 							</div>
 							<div className="card-text">
 								<p>{t('Lady Barbara brengt een unieke smaak door haar mix van rabarber en appel. Zo proef jij haar elegante karakter.')}</p>
-								<h4>{t('Prijs')}: &euro;14,95</h4>
+								<h4 style={{fontSize: '14px'}}>{t('Prijs')}: &euro;14,95</h4>
 							</div>
 						</div>
 					</div>
@@ -34,11 +34,11 @@ const Products = ({t}) => {
 			</div>
 			<div className="card" style={{margin: "1em 0"}}>
 				<div className="row">
-					<div className="d-flex justify-content-center col-4">
+					<div className="d-flex justify-content-center col-5">
 						<img src="Images/Madam Cherry.png" className="card-img" alt="Madam Cherry"
 							 style={{height: "14em", width: 'auto', objectFit: "cover"}}/>
 					</div>
-					<div className="col-8">
+					<div className="col-7">
 						<div className="card-body">
 							<div className="card-title d-flex flex-row-reverse">
 								<span style={{color: "#C90404", fontWeight: "bolder", fontSize: "24px"}}>
@@ -49,7 +49,7 @@ const Products = ({t}) => {
 							</div>
 							<div className="card-text">
 								<p>{t('Maak kennis met Madam Cherry: zij brengt de zoete liefde van appel en kers tot bij jou. Met haar sterk karakter verovert zij jouw hart.')}</p>
-								<h4>{t('Prijs')}: &euro;14,95</h4>
+								<h4 style={{fontSize: '14px'}}>{t('Prijs')}: &euro;14,95</h4>
 								<div className="card-title d-flex flex-row-reverse">
 								</div>
 							</div>
@@ -59,11 +59,11 @@ const Products = ({t}) => {
 			</div>
 			<div className="card" style={{margin: "1em 0"}}>
 				<div className="row">
-					<div className="d-flex justify-content-center col-4">
+					<div className="d-flex justify-content-center col-5">
 						<img src="Images/Fran.png" className="card-img" alt="Duchess Fran"
 							 style={{height: '14em', width:'auto', objectFit: 'cover'}}/>
 					</div>
-					<div className="col-8">
+					<div className="col-7">
 						<div className="card-body">
 							<div className="card-title d-flex flex-row-reverse">
 								<span style={{color: "#E6007E", fontWeight: "bolder", fontSize: "24px"}}>
@@ -73,7 +73,7 @@ const Products = ({t}) => {
 								</span>
 							</div>
 							<p>{t('Duchess Fran, deze chique dame neemt jou mee op een smaakvolle audiëntie. Een koninklijke combinatie van framboos en appel.')}</p>
-							<h4>{t('Prijs')}: &euro;14,95</h4>
+							<h4 style={{fontSize: '14px'}}>{t('Prijs')}: &euro;14,95</h4>
 						</div>
 					</div>
 				</div>
@@ -82,11 +82,11 @@ const Products = ({t}) => {
             
                                  			<div className="card" style={{margin: "1em 0"}}>
 				<div className="row">
-					<div className="d-flex justify-content-center col-4">
+					<div className="d-flex justify-content-center col-5">
 						<img src="Images/MissGinger.png" className="card-img" alt="Miss Cherry"
 							 style={{height: "14em", width:'auto', objectFit: "cover"}}/>
 					</div>
-					<div className="col-8">
+					<div className="col-7">
 						<div className="card-body">
 							<div className="card-title d-flex flex-row-reverse">
 								<span style={{color: "#ECC814", fontWeight: "bolder", fontSize: "24px"}}>
@@ -97,7 +97,7 @@ const Products = ({t}) => {
 							</div>
 							<div className="card-text">
 								<p>{t('Deze knappe dame is Miss Ginger. Zij overtuigt jou met haar verfrissende mix van appel, gember en een subtiele citrustoets.')}</p>
-								<h4>{t('Prijs')}: &euro;14,95</h4>
+								<h4 style={{fontSize: '14px'}}>{t('Prijs')}: &euro;14,95</h4>
 							</div>
 						</div>
 					</div>
@@ -105,11 +105,11 @@ const Products = ({t}) => {
 			</div>
 			<div className="card" style={{margin: '1em 0'}}>
 				<div className="row">
-					<div className="d-flex justify-content-center align-items-center col-4">
+					<div className="d-flex justify-content-center align-items-center col-5">
 						<img src="Images/banner.jpeg" className="card-img" alt="Sfeerfoto"
 							 style={{height: '12em', width:'12em', objectFit: 'cover'}}/>
 					</div>
-					<div className="col-8">
+					<div className="col-7">
 						<div className="card-body">
 								<div className="card-title d-flex flex-row-reverse">
 									<span style={{color: "gray", fontWeight: "bolder", fontSize: "24px"}}>{t('Taste Box')}</span>
@@ -120,7 +120,7 @@ const Products = ({t}) => {
 									4 x {t('Tonic')} (25cl) <br/>
 									2 x {t('EtOH glas')} <br/>
 									{t('verrassing')} <br/></p>
-								<h4>{t('Prijs')}: &euro;24,95</h4>
+								<h4 style={{fontSize: '14px'}}>{t('Prijs')}: &euro;24,95</h4>
 								</div>
 							</div>
 						</div>
